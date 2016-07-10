ff2f4cfb0ef2cf3574acab8f4816985269965cdf
imlonghao <shield@fastmail.com>
2016-07-11 00:40:58 +0800

    Fix the problem when updating the express log

--- crawler.py
+++ crawler.py
@@ -12,20 +12,22 @@ bot = Bot(TELEGRAM_BOT_TOKEN)
 
 for user in r.table('users').run(db):
     for subscribe in user['value']:
-        for express in r.table('traces').get(subscribe).filter(r.row['state'] not in ['3', '4']).run(db):
-            data = requests.get('http://m.kuaidi100.com/query?type=%s&postid=%s&id=1&valicode=&temp=%s' % (
-                express['com'], express['id'], str(random.random()))).json()
-            if express['data'] == data['data']:
-                continue
-            data['id'] = data['nu']
-            del data['nu']
-            for each in data['data']:
-                each['time'] = r.epoch_time(time.mktime(time.strptime(each['time'], '%Y-%m-%d %H:%M:%S')))
-            r.table('traces').get(express['id']).update(data).run(db)
-            text = '快递 {0} 有更新\n\n'
-            for i in data['data'][:len(data['data']) - len(express['data'])]:
-                text += '{0}\n{1}'.format(
-                    i['context'],
-                    i['ftime']
-                )
-            bot.sendMessage(user['id'], text)
+        express = r.table('traces').get(subscribe).run(db)
+        if express['state'] in ['3', '4']:
+            continue
+        data = requests.get('http://m.kuaidi100.com/query?type=%s&postid=%s&id=1&valicode=&temp=%s' % (
+            express['com'], express['id'], str(random.random()))).json()
+        if express['data'] == data['data']:
+            continue
+        data['id'] = data['nu']
+        del data['nu']
+        for each in data['data']:
+            each['time'] = r.epoch_time(time.mktime(time.strptime(each['time'], '%Y-%m-%d %H:%M:%S')))
+        r.table('traces').get(express['id']).update(data).run(db)
+        text = '快递 {0} 有更新\n\n'
+        for i in data['data'][:len(data['data']) - len(express['data'])]:
+            text += '{0}\n{1}'.format(
+                i['context'],
+                i['ftime']
+            )
+        bot.sendMessage(user['id'], text)
